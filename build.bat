30830876f9d2060c52ac90710931d9b96ef5224f
mpcjanssen <mpcjanssen>
2013-06-05 08:16:14 +0000

    Added build script for windows

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Build script for windows
+set JAVA_HOME=C:\Progra~1\Java\jdk1.7.0_01
+set PATH=%PATH%;C:\Users\A156712\Documents\Android\adt-bundle\sdk\platform-tools
+..\adt-bundle\apache-ant-1.9.1\bin\ant %1 %2 %3 %4 %5 %6
