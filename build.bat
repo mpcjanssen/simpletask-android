defaf02bae73d880f3fe6112e784af929bafd836
mpcjanssen <mpcjanssen@noemail.net>
2013-07-04 18:57:58 +0000

    Don't track build.bat it's system specific
    FossilOrigin-Name: c69db01a0c662ecfde0965e8fc2ab9ac6b24ab941101ab89182abd2312cfb022

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Build script for windows
-set JAVA_HOME=C:\Docs\Dropbox\PortableApps\PortableApps\jdk
-set PATH=%PATH%;C:\Docs\android-sdk\platform-tools
-C:\Docs\Dropbox\PortableApps\PortableApps\ant\bin\ant %1 %2 %3 %4 %5 %6
