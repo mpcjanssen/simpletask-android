e2c74decdee2a66afe6fa8d8c014e487cf0b1cf6
mpcjanssen <mpcjanssen@noemail.net>
2013-07-21 11:06:48 +0000

    More tests
    FossilOrigin-Name: 9aed4fcb504c20e10fb5f3a1025f73e3888864853bfd9be5d7440675282e6e03

--- tests/src/nl/mpcjanssen/simpletask/TaskTest.java
+++ tests/src/nl/mpcjanssen/simpletask/TaskTest.java
@@ -18,7 +18,11 @@ public class TaskTest extends TestCase {
     public void testEquals() throws Exception {
         Task a = new Task(1, "Test");
         Task b = new Task(1, "Test");
+        Task c = new Task(1, "Test ");
+        Task d = new Task(2, "Test");
         assertNotSame(a,b);
         assertEquals(a,b);
+        assertFalse(b.equals(c));
+        assertFalse(b.equals(d));
     }
 }
