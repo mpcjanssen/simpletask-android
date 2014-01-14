dbd53ce00d1cd4961307ddd122b5097b72fc490e
Mark Janssen <mpc.janssen@gmail.com>
2014-01-14 13:12:15 +0100

    Also Add Task correctly when cloudless and dropbox versions are installed

--- src/main/java/nl/mpcjanssen/simpletask/Constants.java
+++ src/main/java/nl/mpcjanssen/simpletask/Constants.java
@@ -54,13 +54,13 @@ public class Constants {
     public final static String BROADCAST_UPDATE_UI = "UPDATE_UI";
     public final static String BROADCAST_SYNC_START = "SYNC_START";
     public final static String BROADCAST_SYNC_DONE = "SYNC_DONE";
+    public final static String BROADCAST_ADD_TASK_FROM_WIDGET = ".ADD_TASK_FROM_WIDGET";
 
 
     // Public intents
     public final static String INTENT_START_FILTER = "nl.mpcjanssen.simpletask.START_WITH_FILTER";
     public final static String INTENT_BACKGROUND_TASK = "nl.mpcjanssen.simpletask.BACKGROUND_TASK";
     public final static String EXTRA_BACKGROUND_TASK = "task";
-    public final static String INTENT_ADD_TASK_FROM_WIDGET = "nl.mpcjanssen.simpletask.ADD_TASK_FROM_WIDGET";
 
     // Android OS specific constants
     public static final String ANDROID_EVENT = "vnd.android.cursor.item/event";
