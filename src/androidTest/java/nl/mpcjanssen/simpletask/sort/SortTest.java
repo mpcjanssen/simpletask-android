46d0f90d5c8e23de26efe5770770c630470331f1
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2014-06-02 07:41:39 +0000

    Added failing test for alphabetical sort
    FossilOrigin-Name: 9ead79e972ecfc5b27038ff98d55c9eda5287c5b72bc648501c4d7282d001ef5

---
+++ src/androidTest/java/nl/mpcjanssen/simpletask/sort/SortTest.java
@@ -0,0 +1,35 @@
+package nl.mpcjanssen.simpletask.sort;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TimeZone;
+
+import hirondelle.date4j.DateTime;
+import nl.mpcjanssen.simpletask.task.Priority;
+import nl.mpcjanssen.simpletask.task.Task;
+
+public class SortTest extends TestCase {
+    public void testAlphabeticalSort1 () {
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        Task t1 = new Task(0,"2011-01-01 B");
+        Task t2 = new Task(0,"2012-01-01 A");
+        tasks.add(t1);
+        tasks.add(t2);
+        assertSame(t1, tasks.get(0));
+        Collections.sort(tasks, new AlphabeticalComparator(false));
+        assertSame(t2.inFileFormat(), tasks.get(0).inFileFormat());
+    }
+
+    public void testAlphabeticalSort2 () {
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        Task t1 = new Task(0,"(A) B");
+        Task t2 = new Task(0,"(B) A");
+        tasks.add(t1);
+        tasks.add(t2);
+        assertSame(t1, tasks.get(0));
+        Collections.sort(tasks, new AlphabeticalComparator(false));
+        assertSame(t2.inFileFormat(), tasks.get(0).inFileFormat());
+    }
+}
