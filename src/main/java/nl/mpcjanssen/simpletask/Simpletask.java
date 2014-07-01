525cdd9494a03fccc49368cd0cbf1c4d410f61e5
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2014-07-01 15:16:09 +0000

    Fixed recurrence?
    FossilOrigin-Name: 05e9bf1991d43ab5f965dfe6da748a9b5b55f5ee9954356b55820cdf3a052b47

--- src/main/java/nl/mpcjanssen/simpletask/Simpletask.java
+++ src/main/java/nl/mpcjanssen/simpletask/Simpletask.java
@@ -507,7 +507,7 @@ public class Simpletask extends ThemedListActivity implements AdapterView.OnItem
         for (Task t : tasks) {
             if (t != null && !t.isCompleted()) {
                 if (t.getRecurrencePattern() != null) {
-                    Task newTask = getTaskBag().addAsTask(t.withoutCreateAndCompletionDate());
+                    Task newTask = new Task(0,t.withoutCreateAndCompletionDate());
                     boolean fromOriginalDate = m_app.hasRecurOriginalDates();
                     if (newTask.getDueDate() == null && newTask.getThresholdDate() == null) {
                         newTask.deferDueDate(t.getRecurrencePattern(), fromOriginalDate);
@@ -519,6 +519,7 @@ public class Simpletask extends ThemedListActivity implements AdapterView.OnItem
                             newTask.deferThresholdDate(t.getRecurrencePattern(), fromOriginalDate);
                         }
                     }
+                    getTaskBag().append(newTask.inFileFormat());
                 }
             }
         }
