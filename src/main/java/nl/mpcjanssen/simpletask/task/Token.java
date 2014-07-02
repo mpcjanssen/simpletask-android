1041145239ed9e50dacea321c1cce9ddab01ecbe
Mark Janssen <mpc.janssen@gmail.com>
2014-07-02 10:05:47 +0200

    Token as seperate class

---
+++ src/main/java/nl/mpcjanssen/simpletask/task/Token.java
@@ -0,0 +1,45 @@
+package nl.mpcjanssen.simpletask.task;
+
+/**
+ * Created Mark Janssen
+ */
+class Token {
+    static final String WHITE_SPACE = "WS";
+    static final String LIST = "LIST";
+    static final String TAG = "TAG";
+    static final String COMPLETED = "COMPLETED";
+    static final String TEXT = "TEXT";
+    public String type;
+    public String value;
+    public Object objValue;
+    public Token (String type, String value, Object objValue) {
+        this.type = type;
+        this.value = value;
+        this.objValue = objValue;
+    }
+
+    @Override
+    public String toString() {
+        return type + ":'" + value + "'";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Token token = (Token) o;
+
+        if (!type.equals(token.type)) return false;
+        if (!value.equals(token.value)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type.hashCode();
+        result = 31 * result + value.hashCode();
+        return result;
+    }
+}
