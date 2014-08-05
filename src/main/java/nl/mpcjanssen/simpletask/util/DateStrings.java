5b536f6f39cb958de6886e2fa6f6f230df5473a7
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2014-08-05 15:22:07 +0000

    Also add tasks without prepended date enabled
    FossilOrigin-Name: 20160f7546458227b1398f76ce30d4bbe1d55a9f6d2c3393908a092667b3ee2f

--- src/main/java/nl/mpcjanssen/simpletask/util/DateStrings.java
+++ src/main/java/nl/mpcjanssen/simpletask/util/DateStrings.java
@@ -15,28 +15,28 @@ public class DateStrings {
     public String days_ago;
     public String today;
 
-    public DateStrings(Context context) {
-        this.one_year_ago = context.getString(R.string.dates_one_year_ago);
-        this.years_ago = context.getString(R.string.dates_years_ago, "");
-        this.one_month_ago = context.getString(R.string.dates_one_month_ago);
-        this.months_ago = context.getString(R.string.dates_months_ago, "");
-        this.one_week_ago =context.getString(R.string.dates_one_week_ago);
-        this.weeks_ago = context.getString(R.string.dates_weeks_ago, "");
-        this.one_day_ago = context.getString(R.string.dates_one_day_ago);
-        this.days_ago =  context.getString(R.string.dates_days_ago, "");
-        this.today  = context.getString(R.string.dates_today);
-    }
+    public DateStrings(Context context, int amount) {
+        if (context!=null) {
+            this.one_year_ago = context.getString(R.string.dates_one_year_ago);
+            this.years_ago = context.getString(R.string.dates_years_ago, amount);
+            this.one_month_ago = context.getString(R.string.dates_one_month_ago);
+            this.months_ago = context.getString(R.string.dates_months_ago, amount);
+            this.one_week_ago =context.getString(R.string.dates_one_week_ago);
+            this.weeks_ago = context.getString(R.string.dates_weeks_ago, amount);
+            this.one_day_ago = context.getString(R.string.dates_one_day_ago);
+            this.days_ago =  context.getString(R.string.dates_days_ago, amount);
+            this.today  = context.getString(R.string.dates_today);
+        } else {
 
-    public DateStrings () {
-        this.one_year_ago = "year ago";
-        this.years_ago = "years ago";
-        this.one_month_ago ="month ago";
-        this.months_ago = "months ago";
-        this.one_week_ago ="week ago";
-        this.weeks_ago = "weeks ago";
-        this.one_day_ago = "day ago";
-        this.days_ago =  "days ago";
-        this.today  = "today";
+            this.one_year_ago = "1 year ago";
+            this.years_ago = "" + amount + " years ago";
+            this.one_month_ago = "1 month ago";
+            this.months_ago = "" + amount + "months ago";
+            this.one_week_ago = "1 week ago";
+            this.weeks_ago = "" + amount + " weeks ago";
+            this.one_day_ago = "1 day ago";
+            this.days_ago =  "" + amount + " days ago";
+            this.today  = "today";
+        }
     }
-
 }
