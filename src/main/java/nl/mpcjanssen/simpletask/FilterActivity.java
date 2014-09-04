a27f7623aa6cf96852179fc6cf6309099c23b180
Mark Janssen <mpc.janssen@gmail.com>
2014-09-04 09:41:43 +0200

    Fix bug #135

--- src/main/java/nl/mpcjanssen/simpletask/FilterActivity.java
+++ src/main/java/nl/mpcjanssen/simpletask/FilterActivity.java
@@ -98,7 +98,7 @@ public class FilterActivity extends ThemedActivity {
         // Fill arguments for fragment
         arguments = new Bundle();        
         arguments.putStringArrayList(FILTER_ITEMS, 
-                Util.sortWithPrefix(m_app.getTaskCache().getContexts(), m_app.sortCaseSensitive(), null));
+                Util.sortWithPrefix(m_app.getTaskCache().getContexts(), m_app.sortCaseSensitive(), "-"));
         arguments.putStringArrayList(INITIAL_SELECTED_ITEMS, mFilter.getContexts());
         arguments.putBoolean(INITIAL_NOT, mFilter.getContextsNot());
         actionbar.addTab(actionbar.newTab()
@@ -109,7 +109,7 @@ public class FilterActivity extends ThemedActivity {
         // Fill arguments for fragment
         arguments = new Bundle();
         arguments.putStringArrayList(FILTER_ITEMS, 
-                Util.sortWithPrefix(m_app.getTaskCache().getProjects(), m_app.sortCaseSensitive(), null));
+                Util.sortWithPrefix(m_app.getTaskCache().getProjects(), m_app.sortCaseSensitive(), "-"));
         arguments.putStringArrayList(INITIAL_SELECTED_ITEMS, mFilter.getProjects());
         arguments.putBoolean(INITIAL_NOT, mFilter.getProjectsNot());
         actionbar.addTab(actionbar.newTab()
