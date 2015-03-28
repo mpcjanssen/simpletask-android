69556b85342a0284eee881d0248fcb408a4e71d7
Mark Janssen <mpc.janssen@gmail.com>
2015-03-28 11:01:57 +0100

    Updated author on CalendarSync file

--- src/main/java/nl/mpcjanssen/simpletask/CalendarSync.java
+++ src/main/java/nl/mpcjanssen/simpletask/CalendarSync.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License along with Sinpletask.  If not, see
  * <http://www.gnu.org/licenses/>.
  *
- * @author Todo.txt contributors <todotxt@yahoogroups.com>
+ * @author Vojtech Kral
  * @license http://www.gnu.org/licenses/gpl.html
  * @copyright 2015 Vojtech Kral
  */
