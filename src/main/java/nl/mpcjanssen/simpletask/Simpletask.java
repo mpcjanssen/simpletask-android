cede5adb8ff8f95c101b4ec4debe3d697a7d36d9
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2015-07-24 12:38:28 +0000

    Clean some dead code
    FossilOrigin-Name: f6f1453ea88d761962520260ea5cef4da2ff450a7e1e98ffa21ea05942ad6c6b

--- src/main/java/nl/mpcjanssen/simpletask/Simpletask.java
+++ src/main/java/nl/mpcjanssen/simpletask/Simpletask.java
@@ -1726,8 +1726,6 @@ public class Simpletask extends ThemedActivity implements
             setIntent(intent);
             closeSelectionMode();
             m_adapter.setFilteredTasks();
-            //adapter.notifyDataSetChanged();
         }
     }
-
 }
