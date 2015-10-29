80681332638c11a4b240e6938fd1fc0bbb76c7a2
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2015-10-29 08:05:12 +0000

    Give proper focus to SearchView. Fixes #318
    FossilOrigin-Name: c23c74761938608c8b10c9e732215b2d49d987cf73d59d908165248661e22779

--- src/main/java/nl/mpcjanssen/simpletask/Simpletask.java
+++ src/main/java/nl/mpcjanssen/simpletask/Simpletask.java
@@ -26,12 +26,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.provider.CalendarContract.Events;
+import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -217,7 +218,7 @@ public class Simpletask extends ThemedActivity implements
             return false;
         }
         MenuItem searchMenuItem = options_menu.findItem(R.id.search);
-        searchMenuItem.expandActionView();
+        MenuItemCompat.expandActionView(searchMenuItem);
 
         return true;
     }
