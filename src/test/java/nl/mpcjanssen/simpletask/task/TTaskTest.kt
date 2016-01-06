95b51169a791dd0fbbc2cfbd93e039cd2b9f66f5
Mark Janssen <mpc.janssen@gmail.com>
2016-01-06 18:41:51 +0100

    Add good lexer to split task lines for parsing

--- src/test/java/nl/mpcjanssen/simpletask/task/TTaskTest.kt
+++ src/test/java/nl/mpcjanssen/simpletask/task/TTaskTest.kt
@@ -1,10 +1,38 @@
 package nl.mpcjanssen.simpletask.task
 
 import junit.framework.TestCase
+import java.util.*
 
 class TTaskTest : TestCase() {
     fun testParseIdempotence() {
         val t1 = "x 1231 2 123 312 +test"
         assertEquals(t1, TTask(t1).text)
+        val s = "Test abcd "
+        val t = " "
+        val v = ""
+        assertEquals(s, TTask(s).text)
+        assertEquals(t, TTask(t).text)
+        assertEquals(v, TTask(v).text)
     }
+
+    fun testLexing() {
+        val res = ArrayList<String>()
+        var lexeme = ""
+        "ab b  d s".forEach { char ->
+            when (char) {
+                ' ' -> {
+                    if (lexeme.isNotEmpty()) res.add(lexeme)
+                    res.add(char.toString())
+                    lexeme = ""
+                }
+                else -> lexeme += char
+            }
+        }
+        if (lexeme.isNotEmpty()) res.add(lexeme)
+
+        assertEquals(listOf("ab", " ", "b", " ", " ", "d", " ", "s"),res)
+
+    }
+
+
 }
