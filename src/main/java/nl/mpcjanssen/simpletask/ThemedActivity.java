281f35cbf92449a5d29dc8c2cb9fca0e56bfe5bf
Mark Janssen <mpc.janssen@gmail.com>
2016-01-21 18:43:55 +0100

    Moved preferences to kotlin

--- src/main/java/nl/mpcjanssen/simpletask/ThemedActivity.java
+++ src/main/java/nl/mpcjanssen/simpletask/ThemedActivity.java
@@ -3,7 +3,7 @@ package nl.mpcjanssen.simpletask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-abstract class ThemedActivity extends AppCompatActivity {
+public abstract class ThemedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
