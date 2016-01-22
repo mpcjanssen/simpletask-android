ed3500911baf5edc425dd9d325e2392585416445
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2016-01-22 22:36:32 +0000

    Use lazy task field
    FossilOrigin-Name: 4a6d16984baf0dc0a18408a14f5108a686bd6127613c48aeb74f086f91c690e7

--- src/main/java/nl/mpcjanssen/simpletask/dao/CachedTask.kt
+++ src/main/java/nl/mpcjanssen/simpletask/dao/CachedTask.kt
@@ -7,11 +7,5 @@ import nl.mpcjanssen.simpletask.task.Task
  */
 abstract class CachedTask {
     abstract var text: String
-    private var cachedTask: Task? = null
-    val task: Task
-    get() {
-        val result = cachedTask ?: Task(text)
-        cachedTask = result
-        return result
-    }
+    val task : Task by lazy {Task(text)}
 }
