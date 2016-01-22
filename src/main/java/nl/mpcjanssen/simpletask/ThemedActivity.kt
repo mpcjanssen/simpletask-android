b442ff7647bf1997db15aa35fe43bb6dbbb41ea8
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2016-01-22 15:45:14 +0000

    More kotlin
    FossilOrigin-Name: 929e495543fc3e74b39b4b25856e166f348c66fce08c78803cfff1ad903cfd5b

---
+++ src/main/java/nl/mpcjanssen/simpletask/ThemedActivity.kt
@@ -0,0 +1,14 @@
+package nl.mpcjanssen.simpletask
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+abstract class ThemedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val app = application as TodoApplication
+        setTheme(app.activeTheme)
+        setTheme(app.activeFont)
+        super.onCreate(savedInstanceState)
+    }
+}
