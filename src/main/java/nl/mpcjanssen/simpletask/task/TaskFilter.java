6bb9531b159395902933d75d8c5ae919138bc3ce
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2016-01-24 15:24:15 +0000

    Clear warnings
    FossilOrigin-Name: 661873337409a98ef1bd41b8ce47a97af071b847ecce3304b291f4b1c63b9c9b

--- src/main/java/nl/mpcjanssen/simpletask/task/TaskFilter.java
+++ src/main/java/nl/mpcjanssen/simpletask/task/TaskFilter.java
@@ -1,12 +1,5 @@
 package nl.mpcjanssen.simpletask.task;
 
-/**
- * Created with IntelliJ IDEA.
- * User: A156712
- * Date: 22-1-13
- * Time: 12:25
- * To change this template use File | Settings | File Templates.
- */
 public interface TaskFilter {
     boolean apply(Task t);
 }
