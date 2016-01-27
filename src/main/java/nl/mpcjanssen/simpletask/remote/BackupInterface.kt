820cc004fbaf93890da955aebfdda8e6b8df79ab
Mark Janssen <mpc.janssen@gmail.com>
2016-01-27 20:45:07 +0100

    More kotlin

--- src/main/java/nl/mpcjanssen/simpletask/remote/BackupInterface.kt
+++ src/main/java/nl/mpcjanssen/simpletask/remote/BackupInterface.kt
@@ -1,11 +1,10 @@
-package nl.mpcjanssen.simpletask.remote;
+package nl.mpcjanssen.simpletask.remote
 
 /**
  * Interface definition of the storage backend used.
- *
+
  * Uses events to communicate with the application. Currently supported are SYNC_START, SYNC_DONE and FILE_CHANGED.
  */
-public interface BackupInterface {
-
-    void backup (String name, String contents);
+interface BackupInterface {
+    fun backup(name: String, contents: String)
 }
---
+++ src/main/java/nl/mpcjanssen/simpletask/remote/FileStoreInterface.kt
@@ -0,0 +1,45 @@
+package nl.mpcjanssen.simpletask.remote
+
+import android.app.Activity
+import java.io.IOException
+
+/**
+ * Interface definition of the storage backend used.
+
+ */
+interface FileStoreInterface {
+    val isAuthenticated: Boolean
+    @Throws(IOException::class)
+    fun loadTasksFromFile(path: String, backup: BackupInterface?, eol: String): List<String>
+
+    fun startLogin(caller: Activity, i: Int)
+    fun logout()
+    fun browseForNewFile(act: Activity, path: String, listener: FileSelectedListener, txtOnly: Boolean)
+    @Throws(IOException::class)
+    fun saveTasksToFile(path: String, lines: List<String>, backup: BackupInterface?, eol: String)
+
+    @Throws(IOException::class)
+    fun appendTaskToFile(path: String, lines: List<String>, eol: String)
+
+    val type: Int
+    fun sync()
+    @Throws(IOException::class)
+    fun readFile(file: String, fileRead: FileReadListener): String
+
+    fun supportsSync(): Boolean
+    fun changesPending(): Boolean
+
+    val isLoading: Boolean
+
+    interface FileSelectedListener {
+        fun fileSelected(file: String)
+    }
+
+    interface FileChangeListener {
+        fun fileChanged(newName: String)
+    }
+
+    interface FileReadListener {
+        fun fileRead(contents: String)
+    }
+}
