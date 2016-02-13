cab7f13241c13de8e5b1aff51c29f5944d17fd84
Mark Janssen <mpc.janssen@gmail.com>
2016-02-13 20:19:45 +0100

    Move toodledo keys to config files

--- src/toodledo/java/nl/mpcjanssen/simpletask/remote/OAuthActivity.kt
+++ src/toodledo/java/nl/mpcjanssen/simpletask/remote/OAuthActivity.kt
@@ -9,6 +9,7 @@ import android.webkit.WebViewClient
 import com.github.scribejava.core.builder.ServiceBuilder
 import com.github.scribejava.core.builder.api.DefaultApi20
 import com.github.scribejava.core.model.OAuthConfig
+import com.github.scribejava.core.model.Verifier
 import nl.mpcjanssen.simpletask.HelpScreen
 import nl.mpcjanssen.simpletask.Logger
 import nl.mpcjanssen.simpletask.R
@@ -21,18 +22,18 @@ class OAuthActivity : ThemedActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.logintoodledo)
         val service = ServiceBuilder()
-                .apiKey("")
+                .apiKey(getString(R.string.toodledo_api_client_id))
                 .state("${System.currentTimeMillis()}")
                 .scope("basic tasks")
                 .callback("simpletask://oauth")
-                .apiSecret("")
+                .apiSecret(getString(R.string.toodledo_api_secret))
                 .build(object : DefaultApi20() {
                     override fun getAuthorizationUrl(p0: OAuthConfig?): String? {
                         return "https://api.toodledo.com/3/account/authorize.php?response_type=code&client_id=${p0?.apiKey}&state=${p0?.state}&scope=${p0?.scope}"
                     }
 
                     override fun getAccessTokenEndpoint(): String? {
-                        return "oauth://simpletask"
+                        return "https://api.toodledo.com/3/account/token.php"
                     }
                 } );
         val authorizationUrl = service.authorizationUrl;
@@ -45,7 +46,16 @@ class OAuthActivity : ThemedActivity() {
                 log.debug(TAG, "Loading url: " + url);
                 val uri = Uri.parse(url);
                 if (uri.scheme.equals("simpletask") && uri.host.equals("oauth")) {
-                    log.debug(TAG, "OAuth response received coed = ${uri.getQueryParameters("code")}");
+                    val code = uri.getQueryParameter("code")
+                    val state = uri.getQueryParameter("state")
+                    if (state != service.config.state) {
+                        log.error(TAG, "OAuth State changed from ${service.config.state} to $state, aborting authentication")
+                    }
+
+                    log.debug(TAG, "OAuth response received code = $code")
+                    val verifier = Verifier(code)
+                    val token  = service.getAccessToken(verifier)
+                    log.info(TAG, "Got token ${token}")
                     finish()
                     return false
                 }
