59d81b7800646583be514bdf6d491a8670bde0b8
Mark Janssen <mpc.janssen@gmail.com>
2016-04-13 13:55:05 +0200

    Fix crash if updateLists is opened without selected tasks

---
+++ src/test/java/nl/mpcjanssen/simpletask/util/UtilTest.kt
@@ -0,0 +1,35 @@
+package nl.mpcjanssen.simpletask.util
+
+import junit.framework.TestCase
+
+import nl.mpcjanssen.simpletask.task.Task
+import nl.mpcjanssen.simpletask.task.TodoListItem
+import java.util.*
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mark Janssen
+ * Date: 21-7-13
+ * Time: 12:28
+ */
+
+class UtilTest : TestCase() {
+    fun testAddHeaderLines() {
+        val tasks = ArrayList<Task>()
+        tasks.add(Task("@Home h:1"))
+        tasks.add(Task("@Work h:1"))
+        tasks.add(Task("@College h:1"))
+        val items = ArrayList<TodoListItem>()
+        for (t in tasks) {
+            items.add(TodoListItem(0, t, false))
+        }
+        assertEquals(6, addHeaderLines(items, "by_context", "none").size)
+
+    }
+
+    fun testIntersectUnion() {
+        // Don't crash on an empty lists
+        assertEquals(HashSet<String>(), ArrayList<HashSet<String>>().intersection())
+        assertEquals(HashSet<String>(), ArrayList<HashSet<String>>().union())
+    }
+}
