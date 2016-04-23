a97bb8e09668adfb5c84d724a7227cf60b4c511a
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2016-04-23 10:19:58 +0000

    Allow custom font size between 12sp and 36sp
    FossilOrigin-Name: cd1545f4ad987c620a751e44dfac4a00f78c64dfa00d43a78b71df9c71d75d71

--- src/main/java/nl/mpcjanssen/simpletask/ThemedActivity.kt
+++ src/main/java/nl/mpcjanssen/simpletask/ThemedActivity.kt
@@ -9,7 +9,6 @@ abstract class ThemedActivity : AppCompatActivity() {
         val app = application as TodoApplication
         setTheme(app.activeTheme)
         setTheme(app.activeFont)
-
         super.onCreate(savedInstanceState)
     }
 }
