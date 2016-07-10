37088ccee7d38e17c9e46247fa8a5a9c29959550
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2016-07-10 11:37:25 +0000

    Add missing files
    FossilOrigin-Name: d60e53f0a2e354062486e26ab23a41ec946a81268f92b70b3be12950a0b13ffe

---
+++ src/main/java/nl/mpcjanssen/simpletask/FilterScriptFragment.kt
@@ -0,0 +1,126 @@
+package nl.mpcjanssen.simpletask
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.LinearLayout
+import nl.mpcjanssen.simpletask.task.Task
+import nl.mpcjanssen.simpletask.util.*
+import org.luaj.vm2.LuaError
+
+class FilterScriptFragment : Fragment() {
+    private var txtScript: EditText? = null
+    private var cbUseScript: CheckBox? = null
+    private var txtTestTask: EditText? = null
+    private var log: Logger? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        log = Logger
+        log!!.debug(TAG, "onCreate() this:" + this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log!!.debug(TAG, "onDestroy() this:" + this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        log!!.debug(TAG, "onSaveInstanceState() this:" + this)
+        outState.putString(ActiveFilter.INTENT_SCRIPT_FILTER, script)
+        outState.putString(ActiveFilter.INTENT_SCRIPT_TEST_TASK_FILTER, testTask)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        log!!.debug(TAG, "onCreateView() this:" + this)
+
+        val arguments = arguments
+        log!!.debug(TAG, "Fragment bundle:" + this)
+        val layout = inflater.inflate(R.layout.script_filter,
+                container, false) as LinearLayout
+
+        cbUseScript = layout.findViewById(R.id.cb_use_script) as CheckBox
+        txtScript = layout.findViewById(R.id.txt_script) as EditText
+        txtTestTask = layout.findViewById(R.id.txt_testtask) as EditText
+
+        val btnTest = layout.findViewById(R.id.btnTest) as Button
+        btnTest.setOnClickListener {
+            val t = Task(testTask)
+            try {
+                val script = script
+                LuaScripting.evalScript(script)
+                val snackBar = Snackbar.make(activity.findViewById(android.R.id.content), "", Snackbar.LENGTH_LONG)
+                val barView = snackBar.view
+                if (script.trim { it <= ' ' }.isEmpty() || LuaScripting.onFilterCallback(t)) {
+                    snackBar.setText("True, task will be shown")
+                    barView.setBackgroundColor(0xff43a047.toInt())
+                } else {
+                    snackBar.setText("False: task will not be shown")
+                    barView.setBackgroundColor(0xffe53935.toInt())
+                }
+                snackBar.show()
+            } catch (e: LuaError) {
+                log!!.debug(TAG, "Lua execution failed " + e.message)
+                createAlertDialog(activity, R.string.lua_error, e.message ?: "").show()
+            }
+        }
+        if (savedInstanceState != null) {
+            cbUseScript!!.isChecked = savedInstanceState.getBoolean(ActiveFilter.INTENT_USE_SCRIPT_FILTER, false)
+            txtScript!!.setText(savedInstanceState.getString(ActiveFilter.INTENT_SCRIPT_FILTER, ""))
+            txtTestTask!!.setText(savedInstanceState.getString(ActiveFilter.INTENT_SCRIPT_TEST_TASK_FILTER, ""))
+        } else {
+            cbUseScript!!.isChecked = arguments.getBoolean(ActiveFilter.INTENT_USE_SCRIPT_FILTER, false)
+            txtScript!!.setText(arguments.getString(ActiveFilter.INTENT_SCRIPT_FILTER, ""))
+            txtTestTask!!.setText(arguments.getString(ActiveFilter.INTENT_SCRIPT_TEST_TASK_FILTER, ""))
+        }
+        return layout
+    }
+
+    val useScript: Boolean
+        get() {
+            val arguments = arguments
+            if (cbUseScript == null) {
+                return arguments.getBoolean(ActiveFilter.INTENT_USE_SCRIPT_FILTER, false)
+            } else {
+                return cbUseScript?.isChecked ?: false
+            }
+        }
+
+    var script: String
+        get() {
+            val arguments = arguments
+            if (txtScript == null) {
+                return arguments.getString(ActiveFilter.INTENT_SCRIPT_FILTER, "")
+            } else {
+                return txtScript!!.text.toString()
+            }
+        }
+        set(script) {
+            txtScript?.setText(script)
+        }
+
+
+    val testTask: String
+        get() {
+            val arguments = arguments
+            if (txtTestTask == null) {
+                return arguments.getString(ActiveFilter.INTENT_SCRIPT_TEST_TASK_FILTER, "")
+            } else {
+                return txtTestTask!!.text.toString()
+            }
+        }
+
+    companion object {
+
+        internal val TAG = FilterScriptFragment::class.java.simpleName
+    }
+}
---
+++ src/main/java/nl/mpcjanssen/simpletask/LuaScripting.kt
@@ -0,0 +1,82 @@
+package nl.mpcjanssen.simpletask
+
+import nl.mpcjanssen.simpletask.task.Task
+
+import nl.mpcjanssen.simpletask.util.toDateTime
+import org.luaj.vm2.*
+import org.luaj.vm2.lib.jse.JsePlatform
+import java.util.*
+
+object LuaScripting {
+    private val log = Logger
+    private val TAG = "LuaCallback"
+    val interp = JsePlatform.standardGlobals()
+    val ON_FILTER_NAME = "onFilter"
+
+    fun onFilterCallback (t : Task) : Boolean {
+        val onFilter = interp.get(ON_FILTER_NAME)
+        if (!onFilter.isnil() ) {
+            val args = fillOnFilterVarargs(t)
+            try {
+                val result = onFilter.invoke(args).arg1()
+                return result.toboolean()
+            } catch (e: LuaError) {
+                log.debug(TAG, "Lua execution failed " + e.message)
+            }
+        }
+        return true
+    }
+
+    // Fill the arguments for the onFilter callback
+    fun fillOnFilterVarargs(t: Task): Varargs {
+        val args = ArrayList<LuaValue>();
+        args.add(LuaValue.valueOf(t.inFileFormat()))
+        val fieldTable = LuaTable.tableOf()
+        fieldTable.set("task", t.inFileFormat())
+
+        fieldTable.set("due", dateStringToLuaLong(t.dueDate))
+        fieldTable.set("threshold", dateStringToLuaLong(t.thresholdDate))
+        fieldTable.set("createdate", dateStringToLuaLong(t.createDate))
+        fieldTable.set("completiondate", dateStringToLuaLong(t.completionDate))
+
+        val recPat = t.recurrencePattern
+        if (recPat != null) {
+            fieldTable.set("recurrence", t.recurrencePattern)
+        }
+        fieldTable.set("completed", LuaBoolean.valueOf(t.isCompleted()))
+        fieldTable.set("priority", t.priority.code)
+
+        fieldTable.set("tags", javaListToLuaTable(t.tags))
+        fieldTable.set("lists", javaListToLuaTable(t.lists))
+
+        args.add(fieldTable)
+
+        // TODO: implement
+        val extensionTable = LuaTable.tableOf()
+        args.add(extensionTable)
+
+        return LuaValue.varargsOf(args.toTypedArray())
+    }
+
+    fun evalScript (script : String ) {
+        interp.load( script ).call();
+    }
+
+    fun dateStringToLuaLong(dateString: String?): LuaValue {
+        dateString?.toDateTime()?.let {
+            return LuaValue.valueOf((it.getMilliseconds(TimeZone.getDefault()) / 1000).toDouble())
+        }
+        return LuaValue.NIL
+    }
+
+    fun javaListToLuaTable(javaList: Iterable<String>): LuaValue {
+        val size = javaList.count()
+        if (size == 0) return LuaValue.NIL
+        val luaTable = LuaValue.tableOf()
+        var i = 0
+        for (item in javaList) {
+            luaTable.set(item, LuaValue.TRUE)
+        }
+        return luaTable
+    }
+}
