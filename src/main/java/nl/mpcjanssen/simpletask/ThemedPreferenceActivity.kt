be2c68655227c0dde4a36d513855e306b161d913
Mark Janssen <mpc.janssen@gmail.com>
2016-08-06 20:18:08 +0200

    Fix warnings.

--- src/main/java/nl/mpcjanssen/simpletask/ThemedPreferenceActivity.kt
+++ src/main/java/nl/mpcjanssen/simpletask/ThemedPreferenceActivity.kt
@@ -6,7 +6,6 @@ import nl.mpcjanssen.simpletask.util.Config
 abstract class ThemedPreferenceActivity : AppCompatPreferenceActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val app = application as TodoApplication
         setTheme(Config.activeTheme)
         super.onCreate(savedInstanceState)
     }
