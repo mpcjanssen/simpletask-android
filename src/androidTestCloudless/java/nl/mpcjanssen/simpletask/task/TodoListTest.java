1d3e4ff84b60df7d98afca836e2eb6b798bb23b8
Mark Janssen <mpc.janssen@gmail.com>
2016-08-14 12:06:02 +0200

    Add example instrumentation test for #527

---
+++ src/androidTestCloudless/java/nl/mpcjanssen/simpletask/task/TodoListTest.java
@@ -0,0 +1,24 @@
+package nl.mpcjanssen.simpletask.task;
+
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import java.util.List;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class TodoListTest {
+
+    @Test
+    public void logHistory_ParcelableWriteRead() {
+        // Set up the Parcelable object to send and receive.
+            // Verify that the received data is correct.
+
+        assertTrue(false);
+    }
+}
+
