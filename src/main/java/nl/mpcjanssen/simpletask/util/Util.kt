982680fa8acf6eb565395f9a7c52597faf335b64
Mark Janssen <mpc.janssen@gmail.com>
2016-10-01 14:24:30 +0200

    Reduce font size in help

--- src/main/java/nl/mpcjanssen/simpletask/util/Util.kt
+++ src/main/java/nl/mpcjanssen/simpletask/util/Util.kt
@@ -465,7 +465,7 @@ fun markdownAssetAsHtml(ctxt: Context, name: String): String {
     }
     // Change issue numbers to links
     markdown = markdown.replace("(\\s)(#)([0-9]+)".toRegex(), "$1[$2$3](https://github.com/mpcjanssen/simpletask-android/issues/$3)")
-    html = "<html><head><link rel='stylesheet' type='text/css' href='css/style.css'></head><body>" + Processor.process(markdown) + "</body></html>"
+    html = "<html><head><link rel='stylesheet' type='text/css' href='css/light.css'></head><body>" + Processor.process(markdown) + "</body></html>"
     return html
 }
 
