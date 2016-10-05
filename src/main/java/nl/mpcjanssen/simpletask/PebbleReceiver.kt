b9d16174bc3df7084e63987a7aff03435cc07949
Brandon Knitter <knitterb@blandsite.org>
2016-10-05 15:19:44 -0700

    Handle end of data and n/ack response.

--- src/main/java/nl/mpcjanssen/simpletask/PebbleReceiver.kt
+++ src/main/java/nl/mpcjanssen/simpletask/PebbleReceiver.kt
@@ -19,30 +19,64 @@ class PebbleReceiver(subscribedUuid: UUID?) : PebbleDataReceiver(subscribedUuid)
     override fun receiveData(context: Context?, transactionId: Int, data: PebbleDictionary?) {
         log.debug(TAG, "receiveData...")
 
-        // A new AppMessage was received, tell Pebble
-        log.debug(TAG, "Sending ack...")
-        PebbleKit.sendAckToPebble(context, transactionId);
-        log.debug(TAG, "Ack sent")
 
-        if (data?.getInteger(0) == 0L) {
-            log.info(TAG, "Expected type MESSAGE_KEY_RequestData=0, sending data")
+        val line_val=data?.getInteger(AppKeyRequestData);
+        if (line_val == null || line_val > Int.MAX_VALUE) {
+            log.warn(TAG, "Line number null or too large, cannot send to Pebble: " + line_val)
+
+            // A new AppMessage was received, tell Pebble rejected
+            log.debug(TAG, "Sending ack...")
+            PebbleKit.sendNackToPebble(context, transactionId);
+            log.debug(TAG, "Ack sent")
+        } else {
+            val line: Int = line_val.toInt()
+            log.info(TAG, "Expected type MESSAGE_KEY_RequestData=0, sending data for row " + line);
             val items = TodoList.todoItems
             // TODO(bk) apply filter/sort
-            for (item in items) {
-                val dict = PebbleDictionary()
-                log.debug(TAG, "Sending task: "+item.task.text)
-                dict.addString(AppKeyTaskName, item.task.text)
-                PebbleKit.sendDataToPebble(context, appUuid, dict)
+
+            if (line >= items.size) {
+                log.debug(TAG, "No more items to send...")
+
+                // A new AppMessage was received, tell Pebble
+                log.debug(TAG, "Sending ack...")
+                PebbleKit.sendNackToPebble(context, transactionId);
+                log.debug(TAG, "Nack sent")
+
+
+                return;
             }
-        } else {
-            log.info(TAG, "Unsupported request type, ignoring...")
+
+            // A new AppMessage was received, tell Pebble
+            log.debug(TAG, "Sending ack...")
+            PebbleKit.sendAckToPebble(context, transactionId);
+            log.debug(TAG, "Ack sent")
+
+
+            val item = items.get(line);
+            val dict = PebbleDictionary()
+
+            val text = item.task.text
+
+            dict.addInt32(AppKeyReceiveLine, line);
+            dict.addString(AppKeyReceiveName, text)
+
+            val pebbleIsConnected = PebbleKit.isWatchConnected(context)
+            val pebbleAppMessageSupported = PebbleKit.areAppMessagesSupported(context)
+            log.debug(TAG, "Is watch connected? " + pebbleIsConnected)
+            log.debug(TAG, "Is message supported? " + pebbleAppMessageSupported)
+
+            log.debug(TAG, "Sending task: " + item.line + "=" + item.task.text+" with UUID: "+ appUuid);
+            PebbleKit.sendDataToPebble(context, appUuid, dict)
         }
     }
 
     companion object {
         val appUuid = UUID.fromString("a6c5b8ef-0b0e-4db2-8ebe-32a363699065")
 
-        val AppKeyTaskName=0;
+        val AppKeyRequestData=0;
+
+        val AppKeyReceiveLine=0;
+        val AppKeyReceiveName=1;
 
         private val TAG = "PebbleReceiver"
     }
