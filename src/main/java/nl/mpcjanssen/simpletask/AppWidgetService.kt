mpcjanssen/simpletask-android

commit 343b6d68703d403b7f8126b3c2e3d57f6ed71cba

Remove line element of TodoItem to ensure proper order in written todo.txt.

src/main/java/nl/mpcjanssen/simpletask/AppWidgetService.kt
@@ -55,10 +55,10 @@ data class AppWidgetRemoteViewsFactory(val intent: Intent) : RemoteViewsService.
     }
 
 
-    private fun createSelectedIntent(t: TodoItem): Intent {
+    private fun createSelectedIntent(position: Int): Intent {
         val target = Intent()
         getFilter().saveInIntent(target)
-        target.putExtra(Constants.INTENT_SELECTED_TASK_LINE, t.line)
+        target.putExtra(Constants.INTENT_SELECTED_TASK_LINE, position)
         return target
     }
 
@@ -73,7 +73,7 @@ data class AppWidgetRemoteViewsFactory(val intent: Intent) : RemoteViewsService.
         
         val items = TodoList.todoItems
         visibleTasks.clear()
-        val filter = getFilter() 
+        val filter = getFilter()
 
         for (t in filter.apply(items)) {
             visibleTasks.add(t)
@@ -83,7 +83,13 @@ data class AppWidgetRemoteViewsFactory(val intent: Intent) : RemoteViewsService.
                 TodoApplication.app.today,
                 Config.sortCaseSensitive,
                 filter.createIsThreshold)
-        Collections.sort(visibleTasks, comp)
+
+        if (!comp.fileOrder) {
+            visibleTasks.reverse()
+        }
+        comp.comparators.forEach {
+            Collections.sort(visibleTasks,it)
+        }
         log.debug(TAG, "Widget $widgetId: setFilteredTasks returned ${visibleTasks.size} tasks")
     }
 
@@ -99,7 +105,7 @@ data class AppWidgetRemoteViewsFactory(val intent: Intent) : RemoteViewsService.
         return null
     }
 
-    private fun getExtendedView(item: TodoItem): RemoteViews {
+    private fun getExtendedView(item: TodoItem, position: Int): RemoteViews {
         val filter = getFilter()
         val rv = RemoteViews(TodoApplication.app.packageName, R.layout.widget_list_item)
         val extended_widget = Config.prefs.getBoolean("widget_extended", true)
@@ -183,7 +189,7 @@ data class AppWidgetRemoteViewsFactory(val intent: Intent) : RemoteViewsService.
             //rv.setViewPadding(R.id.tasktext,
             //        4, 4, 4, 0);
         }
-        rv.setOnClickFillInIntent(R.id.taskline, createSelectedIntent(item))
+        rv.setOnClickFillInIntent(R.id.taskline, createSelectedIntent(position))
         return rv
     }
 
@@ -212,7 +218,7 @@ data class AppWidgetRemoteViewsFactory(val intent: Intent) : RemoteViewsService.
         // find index in the to-do list of the clicked task
         val task = visibleTasks[position]
 
-        return getExtendedView(task)
+        return getExtendedView(task,position)
     }
 
 

src/main/java/nl/mpcjanssen/simpletask/sort/MultiComparator.kt
@@ -7,9 +7,10 @@ import nl.mpcjanssen.simpletask.task.TodoItem
 import java.util.*
 import kotlin.comparisons.then
 
-class MultiComparator(sorts: ArrayList<String>, today: String, caseSensitve: Boolean, createAsBackup: Boolean) : Comparator<TodoItem> {
-    var comparators : Comparator<TodoItem>? = null
-    val defaultComparator = FileOrderComparator()
+class MultiComparator(sorts: ArrayList<String>, today: String, caseSensitve: Boolean, createAsBackup: Boolean)  {
+    val comparators = ArrayList<Comparator<TodoItem>>()
+
+    var fileOrder = true
 
     init {
         val log = Logger
@@ -30,7 +31,10 @@ class MultiComparator(sorts: ArrayList<String>, today: String, caseSensitve: Boo
             }
             var comp : Comparator<TodoItem>
             when (sortType) {
-                "file_order" -> comp = FileOrderComparator()
+                "file_order" -> {
+                    fileOrder = !reverse
+                    break@label
+                }
                 "by_context" -> comp = ContextComparator(caseSensitve)
                 "by_project" -> comp = ProjectComparator(caseSensitve)
                 "alphabetical" -> comp = AlphabeticalComparator(caseSensitve)
@@ -49,12 +53,8 @@ class MultiComparator(sorts: ArrayList<String>, today: String, caseSensitve: Boo
             if (reverse) {
                 comp =  CompReverser(comp)
             }
-
-            comparators = comparators?.then(comp) ?:  comp
+            comparators.add(comp)
         }
     }
 
-    override fun compare(o1: TodoItem?, o2: TodoItem?): Int {
-        return comparators?.compare(o1, o2)?:defaultComparator.compare(o1,o2)
-    }
 }

src/main/java/nl/mpcjanssen/simpletask/task/TodoList.kt
@@ -43,7 +43,7 @@ import java.io.IOException
 import java.util.*
 
 
-class TodoItem(val line: Long, val task: Task)
+class TodoItem(val task: Task)
 
 /**
  * Implementation of the in memory representation of the todo list
@@ -83,7 +83,7 @@ object TodoList {
     }
 
     fun add(t: Task, atEnd: Boolean) {
-        val newItem = TodoItem(0, t)
+        val newItem = TodoItem(t)
         add(listOf(newItem), atEnd)
     }
 
@@ -167,7 +167,7 @@ object TodoList {
                 val task = item.task
                 val extra = task.markComplete(todayAsString)
                 if (extra != null) {
-                    val item = TodoItem(0, extra)
+                    val item = TodoItem(extra)
                     if (extraAtEnd) {
                         todoItems.add(item)
                     } else {
@@ -240,7 +240,12 @@ object TodoList {
     fun getSortedTasks(filter: ActiveFilter, sorts: ArrayList<String>, caseSensitive: Boolean): List<TodoItem> {
         val filteredTasks = filter.apply(todoItems)
         val comp = MultiComparator(sorts, TodoApplication.app.today, caseSensitive, filter.createIsThreshold)
-        Collections.sort(filteredTasks, comp)
+        if (!comp.fileOrder) {
+            filteredTasks.reverse()
+        }
+        comp.comparators.forEach {
+            Collections.sort(filteredTasks,it)
+        }
         return filteredTasks
     }
 
@@ -252,7 +257,7 @@ object TodoList {
                 todoItems.clear()
                 val items = ArrayList<TodoItem>(
                         FileStore.loadTasksFromFile(filename, backup, eol).mapIndexed { line, text ->
-                            TodoItem(line.toLong(), Task(text))
+                            TodoItem(Task(text))
                         })
                 todoItems.addAll(items)
                 clearSelection()
@@ -275,9 +280,8 @@ object TodoList {
 
 
     private fun save(fileStore: FileStoreInterface, todoFileName: String, backup: BackupInterface?, eol: String) {
-        val items = todoItems
         try {
-            val lines = items.sortedBy {it.line}.map {
+            val lines = todoItems.map {
                 it.task.inFileFormat()
             }
 
@@ -351,11 +355,6 @@ object TodoList {
     fun getTaskCount(): Long {
         val items = todoItems
         return items.filter { it.task.inFileFormat().isNotBlank() }.size.toLong()
-    }
-
-    fun selectLine(selectedLine : Long ) {
-        clearSelection()
-        selectTodoItem(todoItems.find {selectedLine == it.line})
     }
 
     fun updateCache() {

src/main/java/nl/mpcjanssen/simpletask/AddTaskBackground.kt
@@ -129,7 +129,7 @@ class AddTaskBackground : Activity() {
             } else {
                 t = Task(text)
             }
-            items.add(TodoItem(1, t))
+            items.add(TodoItem(t))
         }
         todoList.add(items, Config.hasAppendAtEnd)
         todoList.notifyChanged(Config.todoFileName, Config.eol, TodoApplication.app, true)

src/main/java/nl/mpcjanssen/simpletask/Simpletask.kt
@@ -309,10 +309,9 @@ class Simpletask : ThemedNoActionBarActivity() {
             MainFilter.initFromPrefs(Config.prefs)
         }
 
-        // Initialize Adapter
-        if (m_adapter == null) {
-            m_adapter = TaskAdapter(layoutInflater)
-        }
+        val adapter = m_adapter ?: TaskAdapter(layoutInflater)
+        m_adapter = adapter
+
         m_adapter!!.setFilteredTasks()
 
         listView?.layoutManager = LinearLayoutManager(this)
@@ -327,11 +326,15 @@ class Simpletask : ThemedNoActionBarActivity() {
         // If we were started from the widget, select the pushed task
         // and scroll to its position
         if (intent.hasExtra(Constants.INTENT_SELECTED_TASK_LINE)) {
-            val line = intent.getLongExtra(Constants.INTENT_SELECTED_TASK_LINE, -1)
+            val position = intent.getIntExtra(Constants.INTENT_SELECTED_TASK_LINE, -1)
             intent.removeExtra(Constants.INTENT_SELECTED_TASK_LINE)
             setIntent(intent)
-            if (line > -1) {
-                TodoList.selectLine(line)
+            if (position > -1) {
+                val itemAtPosition = adapter.getNthTodoItem(position)
+                itemAtPosition?.let {
+                    TodoList.clearSelection()
+                    TodoList.selectTodoItem(itemAtPosition)
+                }
             }
         }
         val selection = TodoList.selectedTasks
@@ -1485,8 +1488,12 @@ class Simpletask : ThemedNoActionBarActivity() {
         ** Get the adapter position for task
         */
         fun getPosition(task: TodoItem): Int {
-            val line = TaskLine(task)
+            val line = TaskLine(task = task)
             return visibleLines.indexOf(line)
+        }
+
+        fun getNthTodoItem(n: Int) : TodoItem? {
+            return visibleLines.filter {!it.header}.getOrNull(n)?.task
         }
 
 

src/main/java/nl/mpcjanssen/simpletask/sort/FileOrderComparator.kt
@@ -1,18 +1,0 @@
-package nl.mpcjanssen.simpletask.sort
-
-import nl.mpcjanssen.simpletask.task.TodoItem
-import java.util.*
-
-
-class FileOrderComparator  : Comparator<TodoItem> {
-    override fun compare(a: TodoItem?, b: TodoItem?): Int {
-        if (a === b) {
-            return 0
-        } else if (a == null) {
-            return -1
-        } else if (b == null) {
-            return 1
-        }
-        return (a.line - b.line).toInt()
-    }
-}

src/main/java/nl/mpcjanssen/simpletask/util/Config.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
-import android.util.JsonReader
-import android.widget.EditText
 import nl.mpcjanssen.simpletask.CalendarSync
 import nl.mpcjanssen.simpletask.LuaInterpreter
 import nl.mpcjanssen.simpletask.R
@@ -13,8 +11,6 @@ import nl.mpcjanssen.simpletask.TodoApplication
 import nl.mpcjanssen.simpletask.remote.FileStore
 import nl.mpcjanssen.simpletask.task.Task
 import nl.mpcjanssen.simpletask.task.TodoItem
-import org.json.JSONArray
-import org.json.JSONObject
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.IOException
@@ -284,7 +280,7 @@ object Config : SharedPreferences.OnSharedPreferenceChangeListener {
 
     var latestChangelogShown: Int
         get() = prefs.getInt(getString(R.string.latest_changelog_shown), 0)
-        set(versionCode: Int) {
+        set(versionCode) {
             prefs.edit().putInt(getString(R.string.latest_changelog_shown), versionCode).commit()
         }
 
@@ -304,8 +300,7 @@ object Config : SharedPreferences.OnSharedPreferenceChangeListener {
                 val stream = ctxt.openFileInput("cachedtodo.txt")
                 val reader = stream.reader(Charset.forName("UTF-8"))
                 val result = ArrayList<TodoItem>()
-                result.addAll(reader.readLines().mapIndexed
-                    { i, line ->  TodoItem(i.toLong(), Task(line))})
+                result.addAll(reader.readLines().map{line ->  TodoItem(Task(line))})
                 reader.close()
                 stream.close()
                 return result

commit c24bdc91ff2b9fd81194ec2eca81d9e4456f214d

Fix sorting breakage caused by 343b6d68703d403b7f8126b3c2e3d57f6ed71cba
FossilOrigin-Name: 52198bf60e527098c9d8a0fe04c8888594ceb74e956e425ed0f90e6999404c95

src/main/java/nl/mpcjanssen/simpletask/AppWidgetService.kt
@@ -87,7 +87,7 @@ data class AppWidgetRemoteViewsFactory(val intent: Intent) : RemoteViewsService.
         if (!comp.fileOrder) {
             visibleTasks.reverse()
         }
-        comp.comparators.forEach {
+        comp.comparator?.let {
             Collections.sort(visibleTasks,it)
         }
         log.debug(TAG, "Widget $widgetId: setFilteredTasks returned ${visibleTasks.size} tasks")

src/main/java/nl/mpcjanssen/simpletask/sort/MultiComparator.kt
@@ -8,7 +8,8 @@ import java.util.*
 import kotlin.comparisons.then
 
 class MultiComparator(sorts: ArrayList<String>, today: String, caseSensitve: Boolean, createAsBackup: Boolean)  {
-    val comparators = ArrayList<Comparator<TodoItem>>()
+    var comparator : Comparator<TodoItem>? = null
+
 
     var fileOrder = true
 
@@ -53,7 +54,7 @@ class MultiComparator(sorts: ArrayList<String>, today: String, caseSensitve: Boo
             if (reverse) {
                 comp =  CompReverser(comp)
             }
-            comparators.add(comp)
+            comparator = comparator?.then(comp) ?:  comp
         }
     }
 

src/main/java/nl/mpcjanssen/simpletask/task/TodoList.kt
@@ -244,7 +244,7 @@ object TodoList {
         if (!comp.fileOrder) {
             filteredTasks.reverse()
         }
-        comp.comparators.forEach {
+        comp.comparator?.let {
             Collections.sort(filteredTasks,it)
         }
         return filteredTasks