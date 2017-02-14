72bb3e081c4c42273af3c0edd33f72642a74c834
Mark Janssen <mpc.janssen@gmail.com>
2017-02-14 10:47:50 +0100

    Add script to publish betas

---
+++ update_beta.sh
@@ -0,0 +1,2 @@
+./gradlew clean build
+cp build/outputs/apk/simpletask-android-cloudless-debug.apk build/outputs/apk/simpletask-android-cloudless-release.apk build/outputs/apk/simpletask-android-dropbox-debug.apk build/outputs/apk/simpletask-android-dropbox-release.apk /home/mpcjanssen/drive/Simpletask/
