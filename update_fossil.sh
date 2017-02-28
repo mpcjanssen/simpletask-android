028025edd88ed9dd6c29923edfa1e1e3f75865e1
Mark Janssen <mpc.janssen@gmail.com>
2017-02-28 17:23:59 +0100

    Separate script for rebuild

--- update_fossil.sh
+++ update_fossil.sh
@@ -1,6 +1,4 @@
 git fast-export bd5fa57c242131100f103b73ce349007d6c188a8..master | fossil import --git .fossil -i
 fossil checkout trunk --keep --force
-./gradlew clean build
-fossil uv add build/outputs/apk/*
 fossil sync -u
 
