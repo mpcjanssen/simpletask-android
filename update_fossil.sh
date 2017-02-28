197e689b9480d462258710c56a374749cb72d4c5
Mark Janssen <mpc.janssen@gmail.com>
2017-02-28 17:18:48 +0100

    Rebuild apks on commit

--- update_fossil.sh
+++ update_fossil.sh
@@ -1,5 +1,6 @@
 git fast-export bd5fa57c242131100f103b73ce349007d6c188a8..master | fossil import --git .fossil -i
+fossil checkout trunk --keep --force
+./gradlew clean build
 fossil uv add build/outputs/apk/*
 fossil sync -u
-fossil checkout trunk --keep --force
 
