15d4ace9be4a7d814e01cc49bcfe8429533bf656
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2017-02-28 16:01:58 +0000

    Update fossil checkout status after git update
    FossilOrigin-Name: 0f08b78b7cbc91e7d90f084a0a9aec7e82bac112c4417e6fa3d8bd77ef4850b9

--- update_fossil.sh
+++ update_fossil.sh
@@ -1,3 +1,5 @@
 git fast-export bd5fa57c242131100f103b73ce349007d6c188a8..master | fossil import --git .fossil -i
 fossil uv add build/outputs/apk/*
 fossil sync -u
+fossil checkout trunk --keep --force
+
