3e136a38991156535262e15ec3944d70e6b21677
Mark Janssen <mpc.janssen@gmail.com>
2017-02-28 16:41:28 +0100

    Removed stale update beta script

--- update_beta.sh
+++
@@ -1,2 +0,0 @@
-./gradlew clean build
-cp build/outputs/apk/simpletask-android-cloudless-debug.apk build/outputs/apk/simpletask-android-cloudless-release.apk build/outputs/apk/simpletask-android-dropbox-debug.apk build/outputs/apk/simpletask-android-dropbox-release.apk /home/mpcjanssen/Nextcloud/Simpletask/
