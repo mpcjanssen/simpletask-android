ca364818d7892df9ca0e00d9acbd0deeae6cb3da
mark Janssen <mpc.janssen@gmail.com>
2017-09-12 13:14:21 +0200

    Added tokens in the field parameters and a `print_table` diagnostic function for use in onDisplay.

--- app/src/main/java/nl/mpcjanssen/simpletask/LuaInterpreter.kt
+++ app/src/main/java/nl/mpcjanssen/simpletask/LuaInterpreter.kt
@@ -19,11 +19,101 @@ object LuaInterpreter {
     val ON_TEXTSEARCH_NAME = "onTextSearch"
     val CONFIG_THEME = "theme"
     val CONFIG_TASKLIST_TEXT_SIZE_SP = "tasklistTextSize"
+    val STDLIB = """
+        function print_table(node)
+    -- to make output beautiful
+    local function tab(amt)
+        local str = ""
+        for i=1,amt do
+            str = str .. "\t"
+        end
+        return str
+    end
+
+    local cache, stack, output = {},{},{}
+    local depth = 1
+    local output_str = "{\n"
+
+    while true do
+        local size = 0
+        for k,v in pairs(node) do
+            size = size + 1
+        end
+
+        local cur_index = 1
+        for k,v in pairs(node) do
+            if (cache[node] == nil) or (cur_index >= cache[node]) then
+
+                if (string.find(output_str,"}",output_str:len())) then
+                    output_str = output_str .. ",\n"
+                elseif not (string.find(output_str,"\n",output_str:len())) then
+                    output_str = output_str .. "\n"
+                end
+
+                -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+                table.insert(output,output_str)
+                output_str = ""
+
+                local key
+                if (type(k) == "number" or type(k) == "boolean") then
+                    key = "["..tostring(k).."]"
+                else
+                    key = "['"..tostring(k).."']"
+                end
+
+                if (type(v) == "number" or type(v) == "boolean") then
+                    output_str = output_str .. tab(depth) .. key .. " = "..tostring(v)
+                elseif (type(v) == "table") then
+                    output_str = output_str .. tab(depth) .. key .. " = {\n"
+                    table.insert(stack,node)
+                    table.insert(stack,v)
+                    cache[node] = cur_index+1
+                    break
+                else
+                    output_str = output_str .. tab(depth) .. key .. " = '"..tostring(v).."'"
+                end
+
+                if (cur_index == size) then
+                    output_str = output_str .. "\n" .. tab(depth-1) .. "}"
+                else
+                    output_str = output_str .. ","
+                end
+            else
+                -- close the table
+                if (cur_index == size) then
+                    output_str = output_str .. "\n" .. tab(depth-1) .. "}"
+                end
+            end
+
+            cur_index = cur_index + 1
+        end
+
+        if (size == 0) then
+            output_str = output_str .. "\n" .. tab(depth-1) .. "}"
+        end
+
+        if (#stack > 0) then
+            node = stack[#stack]
+            stack[#stack] = nil
+            depth = cache[node] == nil and depth + 1 or depth - 1
+        else
+            break
+        end
+    end
+
+    -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+    table.insert(output,output_str)
+    output_str = table.concat(output)
+
+    return output_str
+end
+            """
 
     init {
 
         try {
             globals.set("toast", LuaToastShort())
+            evalScript(null,STDLIB)
             evalScript(null, Config.luaConfig)
 
         } catch (e: LuaError) {
@@ -143,8 +233,15 @@ object LuaInterpreter {
         val args = ArrayList<LuaValue>()
         args.add(LuaValue.valueOf(t.inFileFormat()))
         val fieldTable = LuaTable.tableOf()
+        val tokensTable =  LuaTable.tableOf()
         fieldTable.set("task", t.inFileFormat())
-
+        t.tokens.forEachIndexed { idx,  tok ->
+            val tokenTable =  LuaTable.tableOf()
+            tokenTable.set("type", LuaValue.valueOf(tok.type))
+            tokenTable.set("text", LuaValue.valueOf(tok.text))
+            tokensTable.set(idx, tokenTable)
+        }
+        fieldTable.set("tokens", tokensTable)
         fieldTable.set("due", dateStringToLuaLong(t.dueDate))
         fieldTable.set("threshold", dateStringToLuaLong(t.thresholdDate))
         fieldTable.set("createdate", dateStringToLuaLong(t.createDate))
@@ -211,6 +308,8 @@ object LuaInterpreter {
             module.set(ON_FILTER_NAME, LuaValue.NIL)
         }
     }
+
+
 }
 
 class LuaToastShort : OneArgFunction() {
