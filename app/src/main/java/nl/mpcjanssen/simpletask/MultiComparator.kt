1a8be3cb1742c4ddd0666443fd060d714c236f47
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2017-10-06 12:50:16 +0000

    Use lambdas for sorting.
    FossilOrigin-Name: 736dff88fd8b80d4329c07e5e9f64ec19669d2a8e3ad8a6dfbca137bacac2124

---
+++ app/src/main/java/nl/mpcjanssen/simpletask/MultiComparator.kt
@@ -0,0 +1,65 @@
+package nl.mpcjanssen.simpletask
+
+import nl.mpcjanssen.simpletask.task.TToken
+import nl.mpcjanssen.simpletask.task.Task
+import java.util.*
+
+class MultiComparator(sorts: ArrayList<String>, today: String, caseSensitve: Boolean, createAsBackup: Boolean) {
+    var comparator : Comparator<Task>? = null
+
+    var fileOrder = true
+
+    init {
+        val log = Logger
+
+        label@ for (sort in sorts) {
+            val parts = sort.split(ActiveFilter.SORT_SEPARATOR.toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()
+            var reverse = false
+            val sortType: String
+            if (parts.size == 1) {
+                // support older shortcuts and widgets
+                reverse = false
+                sortType = parts[0]
+            } else {
+                sortType = parts[1]
+                if (parts[0] == ActiveFilter.REVERSED_SORT) {
+                    reverse = true
+                }
+            }
+            var comp: (Task) -> Comparable<*>
+            when (sortType) {
+                "file_order" -> {
+                    fileOrder = !reverse
+                    break@label
+                }
+                "by_context" -> comp = { it.lists.sorted().firstOrNull() ?: "" }
+                "by_project" -> comp = { it.tags.sorted().firstOrNull() ?: "" }
+                "alphabetical" -> comp = if (caseSensitve) {
+                    { it -> it.showParts(TToken.TEXT) }
+                } else {
+                    { it -> it.showParts(TToken.TEXT).toLowerCase(Locale.getDefault()) }
+                }
+                "by_prio" -> comp = { it.priority }
+                "completed" -> comp = { it.isCompleted() }
+                "by_creation_date" -> comp = { it.createDate ?: "" }
+                "in_future" -> comp = { it.inFuture(today) }
+                "by_due_date" -> comp = { it.dueDate ?: "" }
+                "by_threshold_date" -> comp = {
+                    val fallback = if (createAsBackup) it.createDate ?: "" else ""
+                    it.thresholdDate ?: fallback
+                }
+                "by_completion_date" -> comp = { it.completionDate ?: "" }
+                else -> {
+                    log.warn("MultiComparator", "Unknown sort: " + sort)
+                    continue@label
+                }
+            }
+            if (reverse) {
+                comparator = comparator?.thenByDescending(comp) ?: compareByDescending(comp)
+            } else {
+                comparator = comparator?.thenBy(comp) ?: compareBy(comp)
+            }
+        }
+    }
+
+}
