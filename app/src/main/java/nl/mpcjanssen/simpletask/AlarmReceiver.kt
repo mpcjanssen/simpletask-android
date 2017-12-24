6d953757896ce2c1d3c7509b17156cc36562e67f
Mark Janssen <mpc.janssen@gmail.com>
2017-12-24 11:34:19 +0100

    Don't use (possibly) uninitialized `app` field in Alarm callback.

--- app/src/main/java/nl/mpcjanssen/simpletask/AlarmReceiver.kt
+++ app/src/main/java/nl/mpcjanssen/simpletask/AlarmReceiver.kt
@@ -3,17 +3,21 @@ package nl.mpcjanssen.simpletask
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.support.v4.content.LocalBroadcastManager
 import nl.mpcjanssen.simpletask.dao.Daos
 import nl.mpcjanssen.simpletask.task.TodoList
+import nl.mpcjanssen.simpletask.util.broadcastFileSync
+import nl.mpcjanssen.simpletask.util.broadcastRefreshUI
 
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+        val lbm = LocalBroadcastManager.getInstance(context)
         Logger.info(TAG, "Executing Alarm callback")
         if (Constants.ALARM_RELOAD == intent.getStringExtra(Constants.ALARM_REASON_EXTRA)) {
-            TodoList.reload(TodoApplication.app, "Reload in background")
+            broadcastFileSync( lbm )
         } else {
-            TodoApplication.app.localBroadCastManager.sendBroadcast(Intent(Constants.BROADCAST_UPDATE_UI))
+            broadcastRefreshUI(lbm)
         }
 
     }
