eb8a8f1f2141d470a4c4fce17c08fe419f2b933c
Mark Janssen <mark.janssen@pon.com>
2018-03-05 16:46:01 +0100

    Remove unneeded Log Dao files.

--- extras/daogenerator/src/nl/mpcjanssen/simpletask/SimpletaskDaoGenerator.java
+++ extras/daogenerator/src/nl/mpcjanssen/simpletask/SimpletaskDaoGenerator.java
@@ -21,25 +21,14 @@ public class SimpletaskDaoGenerator {
 
         addEntities(schema);
 
-        new DaoGenerator().generateAll(schema, "../app/src/main/java");
+        new DaoGenerator().generateAll(schema, "app/src/main/java");
 
     }
 
     private static void addEntities(Schema schema) {
         backupSchema(schema);
-        logSchema(schema);
     }
 
-
-    private static void logSchema(Schema schema) {
-        Entity log = schema.addEntity("LogItem");
-        log.addIdProperty().primaryKey().autoincrement();
-        log.addDateProperty("timestamp").notNull();
-        log.addStringProperty("severity").notNull();
-        log.addStringProperty("tag").notNull();
-        log.addStringProperty("message").notNull();
-        log.addStringProperty("exception").notNull();
-    }
 
     private static void backupSchema(Schema schema) {
         Entity entry = schema.addEntity("TodoFile");
