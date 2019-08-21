e2042bd728d6ffaa5da89b5ceea1edb544ff3e65
mpc.janssen@gmail.com <mpc.janssen@gmail.com@noemail.net>
2019-08-21 09:09:48 +0000

    Fix warnings.
    FossilOrigin-Name: 36c7be74a3841479387ab1fdbe1b8b72409a0ce236b69bcca84888856c952fee

--- app/src/main/java/nl/mpcjanssen/simpletask/FilterListFragment.kt
+++ app/src/main/java/nl/mpcjanssen/simpletask/FilterListFragment.kt
@@ -1,38 +1,32 @@
 package nl.mpcjanssen.simpletask
 
-import android.app.ActionBar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.util.Log
-import android.view.GestureDetector
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import java.util.*
+import kotlinx.android.synthetic.main.multi_filter.*
 import kotlin.collections.ArrayList
 
 class FilterListFragment : Fragment() {
-    private var lv: ListView? = null
-    private var cb: CheckBox? = null
-    private val gestureDetector: GestureDetector? = null
-    internal var actionbar: ActionBar? = null
     private var mSelectedItems: ArrayList<String>? = null
     private var not: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate() this:" + this)
+        Log.d(TAG, "onCreate() this:$this")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG, "onDestroy() this:" + this)
+        Log.d(TAG, "onDestroy() this:$this")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        Log.d(TAG, "onSaveInstanceState() this:" + this)
+        Log.d(TAG, "onSaveInstanceState() this:$this")
         outState.putStringArrayList("selectedItems", getSelectedItems())
         outState.putBoolean("not", getNot())
 
@@ -40,11 +34,10 @@ class FilterListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        Log.d(TAG, "onCreateView() this:" + this + " savedInstance:" + savedInstanceState)
-
-        val arguments = arguments
-        val items = arguments?.getStringArrayList(FilterActivity.FILTER_ITEMS) ?: emptyList<String>()
-        actionbar = activity?.actionBar
+        Log.d(TAG, "onCreateView() this:$this savedInstance:$savedInstanceState")
+        Log.d(TAG, "Fragment bundle:$this arguments:$arguments")
+        val layout = inflater.inflate(R.layout.multi_filter,
+                container, false) as LinearLayout
 
         if (savedInstanceState != null) {
             mSelectedItems = savedInstanceState.getStringArrayList("selectedItems")
@@ -54,55 +47,50 @@ class FilterListFragment : Fragment() {
             not = arguments?.getBoolean(FilterActivity.INITIAL_NOT) ?: false
         }
 
-        Log.d(TAG, "Fragment bundle:" + this + " arguments:" + arguments)
-        val layout = inflater.inflate(R.layout.multi_filter,
-                container, false) as LinearLayout
-
-        cb = layout.findViewById(R.id.checkbox) as CheckBox
-
-        lv = layout.findViewById(R.id.listview) as ListView
-        lv!!.choiceMode = AbsListView.CHOICE_MODE_MULTIPLE
-
-        lv!!.adapter = ArrayAdapter(activity,
-                R.layout.simple_list_item_multiple_choice, items)
-
-        for (i in items.indices) {
-            if (mSelectedItems != null && mSelectedItems!!.contains(items[i])) {
-                lv!!.setItemChecked(i, true)
-            }
-        }
-
-        cb!!.isChecked = not
         return layout
+    }
+    override fun onResume() {
+        super.onResume()
+
+        activity?.let { act ->
+            listview.choiceMode = AbsListView.CHOICE_MODE_MULTIPLE
+            val items = arguments?.getStringArrayList(FilterActivity.FILTER_ITEMS) ?: emptyList<String>()
+            listview.adapter = ArrayAdapter(act,
+                    R.layout.simple_list_item_multiple_choice, items)
+            for (i in items.indices) {
+                if (mSelectedItems != null && mSelectedItems!!.contains(items[i])) {
+                    listview.setItemChecked(i, true)
+                }
+            }
+            checkbox.isChecked = not
+        }
     }
 
     fun getNot(): Boolean {
-        if (mSelectedItems == null) {
+        return if (mSelectedItems == null) {
             // Tab was not displayed so no selections were changed
-            return arguments?.getBoolean(FilterActivity.INITIAL_NOT)?: false
+            arguments?.getBoolean(FilterActivity.INITIAL_NOT)?: false
         } else {
-            return cb!!.isChecked
+            checkbox.isChecked
         }
     }
 
     fun getSelectedItems(): ArrayList<String> {
-
         val arr = ArrayList<String>()
         if (mSelectedItems == null) {
             // Tab was not displayed so no selections were changed
-            return arguments?.getStringArrayList(FilterActivity.INITIAL_SELECTED_ITEMS) ?: ArrayList<String>()
+            return arguments?.getStringArrayList(FilterActivity.INITIAL_SELECTED_ITEMS) ?: ArrayList()
         }
-        val size = lv!!.count
-        for (i in 0..size - 1) {
-            if (lv!!.isItemChecked(i)) {
-                arr.add(lv!!.adapter.getItem(i) as String)
+        val size = listview.count
+        for (i in 0 until size) {
+            if (listview.isItemChecked(i)) {
+                arr.add(listview.adapter.getItem(i) as String)
             }
         }
         return arr
     }
 
     companion object {
-
         internal val TAG = FilterListFragment::class.java.simpleName
     }
 }
