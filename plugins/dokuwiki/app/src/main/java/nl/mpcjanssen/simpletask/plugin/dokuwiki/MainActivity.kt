a0f85000ecad3d6a0effa86adbe146b5b4b4635e
mark.janssen@pon.com <mark.janssen@pon.com@noemail.net>
2020-02-10 11:20:54 +0000

    Add dokuwiki plugin.
    FossilOrigin-Name: 9dd1cc105c496c975d224dbb2c0ad67939c884d69ad68c0d0e98eb10de964fe6

---
+++ plugins/dokuwiki/app/src/main/java/nl/mpcjanssen/simpletask/plugin/dokuwiki/MainActivity.kt
@@ -0,0 +1,16 @@
+package nl.mpcjanssen.simpletask.plugin.dokuwiki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        name.text = packageName
+
+
+    }
+}
---
+++ plugins/dokuwiki/app/src/main/java/nl/mpcjanssen/simpletask/plugin/dokuwiki/MainApplication.kt
@@ -0,0 +1,19 @@
+package nl.mpcjanssen.simpletask.plugin.dokuwiki
+
+import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+
+
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        app = this
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+    }
+    companion object {
+        lateinit var app: Application
+    }
+}
