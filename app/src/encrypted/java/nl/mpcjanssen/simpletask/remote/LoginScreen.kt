c9e046b2be305487d005da6e2ac7460cc5124992
nicdnb <4390789+nicdnb@users.noreply.github.com>
2021-11-04 23:54:36 -0400

    WIP - Encrypted Version

---
+++ app/src/encrypted/java/nl/mpcjanssen/simpletask/remote/LoginScreen.kt
@@ -0,0 +1,97 @@
+/**
+ * This file is part of Todo.txt Touch, an Android app for managing your todo.txt file (http://todotxt.com).
+
+ * Copyright (c) 2009-2012 Todo.txt contributors (http://todotxt.com)
+
+ * LICENSE:
+
+ * Todo.txt Touch is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+
+ * Todo.txt Touch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+
+ * You should have received a copy of the GNU General Public License along with Todo.txt Touch.  If not, see
+ * //www.gnu.org/licenses/>.
+
+ * @author Todo.txt contributors @yahoogroups.com>
+ * *
+ * @license http://www.gnu.org/licenses/gpl.html
+ * *
+ * @copyright 2009-2012 Todo.txt contributors (http://todotxt.com)
+ */
+package nl.mpcjanssen.simpletask.remote
+
+import android.Manifest
+import android.content.Intent
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import nl.mpcjanssen.simpletask.Simpletask
+import nl.mpcjanssen.simpletask.ThemedNoActionBarActivity
+import nl.mpcjanssen.simpletask.TodoApplication
+import nl.mpcjanssen.simpletask.databinding.LoginBinding
+import nl.mpcjanssen.simpletask.util.showToastLong
+
+class LoginScreen : ThemedNoActionBarActivity() {
+
+    private lateinit var binding: LoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (FileStore.isAuthenticated) {
+            switchToTodolist()
+        }
+        setTheme(TodoApplication.config.activeTheme)
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val loginButton = binding.login
+        loginButton.setOnClickListener {
+            startLogin()
+        }
+        val newUserButton = binding.newUser
+        newUserButton.setOnClickListener {
+            startSignUp()
+        }
+    }
+
+    private fun switchToTodolist() {
+        val intent = Intent(this, Simpletask::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun finishLogin() {
+
+        if (FileStore.isAuthenticated) {
+            switchToTodolist()
+        } else {
+            showToastLong(this, "Storage access denied")
+        }
+    }
+
+    private fun startLogin() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_PERMISSION)
+        FileDialog.requestFolder(this)
+    }
+
+    private fun startSignUp() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_PERMISSION)
+        FileDialog.createFile(this)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_PERMISSION -> finishLogin()
+        }
+    }
+
+    companion object {
+        private val REQUEST_PERMISSION = 1
+        internal val TAG = LoginScreen::class.java.simpleName
+    }
+}
