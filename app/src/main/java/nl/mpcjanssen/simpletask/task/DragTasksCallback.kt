c56320d3e17d23a15023a2de132ea5fa42483cad
Michael Struwe <michaelstruwe@gmx.net>
2022-04-20 17:29:10 +0200

    Add some explanatory comments to DragTasksCallback

--- app/src/main/java/nl/mpcjanssen/simpletask/task/DragTasksCallback.kt
+++ app/src/main/java/nl/mpcjanssen/simpletask/task/DragTasksCallback.kt
@@ -30,11 +30,15 @@ class DragTasksCallback(val taskAdapter: TaskAdapter) : ItemTouchHelper.Callback
         return 0
     }
 
+    // Called when the line we're dragging is being swapped with another line
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
         val from = viewHolder.bindingAdapterPosition
         val to = target.bindingAdapterPosition
 
         if (taskAdapter.canMoveVisibleLine(from, to)) {
+            // We don't really save the move to the todo.txt file here.  Instead
+            // we update the current state of what the move is that the user is
+            // doing in total.
             updateCurrentMove(from, to)
             taskAdapter.visuallyMoveLine(from, to)
             return true
@@ -43,6 +47,10 @@ class DragTasksCallback(val taskAdapter: TaskAdapter) : ItemTouchHelper.Callback
         }
     }
 
+    // Called when the dragging is done (We should now clear any decoractions we
+    // added to the view that was being dragged, hence the name.  We mainly use
+    // this callback to persist the move to the todo.txt file now that it's
+    // done.)
     override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
         super.clearView(recyclerView, viewHolder)
 
@@ -69,8 +77,21 @@ class DragTasksCallback(val taskAdapter: TaskAdapter) : ItemTouchHelper.Callback
 
         val oldCurrentMove = currentMove
         if (oldCurrentMove == null) {
+            // If `currentMove` wasn't set before, the "from" task is the task
+            // we're currently dragging.
+            // (Easy refactoring idea: Actually, we don't need to save the
+            // "from" task because it's always the task that is currently being
+            // dragged.  I.e. we can drop both "from" attributes from the
+            // LineMove class and just compute them in `clearView` the way
+            // they're computed here / in `onMove`.  I think this would also
+            // allow us to get rid of the if statement here.)
             val fromLineIndex = nowFrom
             val fromTask = taskAdapter.getMovableTaskAt(nowFrom)
+
+            // Lower indices are higher up in the list.  We look at the current
+            // swapping of two items:  If we come from higher up in the list, we
+            // want the dragging item to be placed below the other item.  If we
+            // come from lower down, then above.
             val isMoveBelow = nowFrom < nowTo
             currentMove = LineMove(fromLineIndex, toLineIndex, fromTask, toTask, isMoveBelow)
         } else {
